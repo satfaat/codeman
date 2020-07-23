5cc2e0151f26c5b6465677be76acff223c563176
satfaat <satfaat@gmail.com>
2020-07-23 10:15:48 +0400

    practice

--- prj_blog/tmp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path  # из библиотеки django.urls импортируем функцию path
-from . import views  # импортируем файл views.py из корня проекта
-
-urlpatterns = [
-    path('', views.index)
-]
