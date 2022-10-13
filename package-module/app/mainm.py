0d1f9c8ea8872d7c13ebebd8c400b50d88dbce48
satfaat <satfaat@gmail.com>
2022-10-13 10:31:10 +0400

    confl

---
+++ package-module/app/mainm.py
@@ -0,0 +1,9 @@
+# import package.module
+    # package.module.function
+
+import calc
+
+
+calc = calc.Calculate
+res = calc.add(3, 5)
+print(res)
---
+++ package-module/main/mainp.py
@@ -0,0 +1,8 @@
+# import package.module
+    # package.module.function
+
+import app.calc
+
+calc = app.calc.Calculate
+res = calc.add(4, 3)
+print(res)
