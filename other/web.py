5fbc27822c0014fcbcc21c40c70ab8a111da6968
satfaat <satfaat@gmail.com>
2022-10-19 14:07:51 +0400

    update

--- other/web.py
+++ other/web.py
@@ -16,7 +16,6 @@ for s in strings:
     decoded = urllib.parse.unquote(encoded)  # расшифрованная обратно строка
     print(decoded, '-', encoded)
 
-import urllib.parse
 
 url = 'https://yandex.ru/search/?text=%D0%BA%D0%B0%D0%BA%20%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE%20%D0%B5%D0%B7%D0%B4%D0%B8%D1%82%D1%8C%20%D0%BD%D0%B0%20%D1%82%D0%B0%D0%BA%D1%81%D0%B8'
 
